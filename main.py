c97a6f12eba60e577efa8616cac6aad32129c28b
Becky Wilson <rebecca.wilson2@sky.uk>
2021-09-01 20:32:54 +0100

    setting API limits to TRUE

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import keys
 
 auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
-
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)

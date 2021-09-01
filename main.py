c5528204888579958dfd2a76691438bd857d8b22
Becky Wilson <rebecca.wilson2@sky.uk>
2021-09-01 20:33:56 +0100

    checking type

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 # Selecting a User
 tweetOfGod = api.get_user('TheTweetOfGod')
 
+# Check what is returned with
+# returns: tweepy.models.User
+type(tweetOfGod)
+
+
